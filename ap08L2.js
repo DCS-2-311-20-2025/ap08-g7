f9561e4858ffb9151dec95f2ec1c408bd1f71cd4
248461@st.takushoku-u.ac.jp <248461@st.takushoku-u.ac.jp>
2025-11-19 15:09:09 +0900

    L2

--- ap08L2.js
+++ ap08L2.js
@@ -20,7 +20,7 @@ export const origin = new THREE.Vector3();
 export const controlPoints = [
     [ 50,-20],
     [-40, 10],
-    [ 20, -35],
+    [ 20,-35],
     [-25, 40]
 ]
 export function init(scene, size, id, offset, texture) {
@@ -81,6 +81,22 @@ export function init(scene, size, id, offset, texture) {
 
 // コース(自動運転用)
 export function makeCourse(scene) {
+    const courseVectors = [];
+    const parts = [L2,L3,L4,L1];
+    parts.forEach((part) => {
+        part.controlPoints.forEach((p) => {
+            courseVectors.push(
+                new THREE.Vector3(
+                    p[0] + part.origin.x,
+                    0,
+                    p[1] + part.origin.z,
+                )
+            )
+        });
+    })
+    course = new THREE.CatmullRomCurve3(
+        courseVectors, true
+    )
 }
 
 // カメラを返す
@@ -104,7 +120,15 @@ export function resize() {
 }
 
 // 描画処理
+const clock = new THREE.Clock();
+const carPosition = new THREE.Vector3();
+const carTarget = new THREE.Vector3();
 export function render(scene, car) {
+    const time = (clock.getElapsedTime() / 20);
+    course.getPointAt(time % 1, carPosition);
+    car.position.copy(carPosition);
+    course.getPointAt((time + 0.01) %1, carTarget);
+    car.lookAt(carTarget);
     camera.lookAt(car.position.x, car.position.y, car.position.z);
     renderer.render(scene, camera);
 }
